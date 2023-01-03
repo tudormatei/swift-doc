cfe6e3cd7c7c31b1fe60e816805ca67d0149333a
tudormatei <tudormatei010@gmail.com>
2023-01-03 11:58:39 +0200

    Can send pdf back now

--- backend/server.py
+++ backend/server.py
@@ -5,7 +5,10 @@ from flask import (Flask, Response, jsonify, request, send_file,
                    send_from_directory)
 from PyPDF2 import PdfReader
 
+UPLOAD_FOLDER = './/backend//documents//'
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
@@ -13,24 +16,23 @@ def index():
 
 
 @app.route('/api/pass', methods=['POST'])
-def predict():
+def modifyPDF():
     try:
         pdf_file = request.files['doc']
         pdf_name = pdf_file.filename
-        save_path='.//backend//documents//' + pdf_name
 
-        #Saves file
-        pdf_file.save(save_path)
+        pdf_file.save(os.path.join(app.config['UPLOAD_FOLDER'] + pdf_name))
+
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'] + pdf_name)
         
-        reader = PdfReader(save_path)
+        reader = PdfReader(file_path)
         page = reader.pages[0]
         print(page.extract_text())
 
-        return "Sucessfully sent pdf file..."
+        return send_file('documents//' + pdf_name)
 
     except Exception as e:
-        app.logger.info("Error occurred")
-
+        return "Error occured"
 
 if __name__ == '__main__':
     app.run(debug=True)
